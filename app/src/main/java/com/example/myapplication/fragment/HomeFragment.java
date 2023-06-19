6c9d9da331d21c1f929e6bb23d99670a56b94b1f
Khmisch <khamraeva.m1124@gmail.com>
2023-06-20 04:25:22 +0800

    commit

--- app/src/main/java/com/example/myapplication/fragment/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/HomeFragment.java
@@ -6,7 +6,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
@@ -15,6 +18,7 @@ import com.example.myapplication.R;
 import com.example.myapplication.adapter.HomeAdapter;
 import com.example.myapplication.helper.SpacesItemDecoration;
 import com.example.myapplication.model.Photo;
+import com.example.myapplication.model.Search;
 import com.example.myapplication.network.RetrofitHttp;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,8 +29,12 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private HomeAdapter adapter;
-    private int currentPage = 1;
-    private int perPage = 30;
+
+    private TextView tv_cities;
+    private TextView tv_cuisine;
+    private TextView tv_festivals;
+    private TextView tv_kungfu;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,26 +48,53 @@ public class HomeFragment extends Fragment {
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         SpacesItemDecoration decoration = new SpacesItemDecoration(10);
         recyclerView.addItemDecoration(decoration);
-        apiPhotoList();
+
+        tv_cities = view.findViewById(R.id.tv_cities);
+        tv_cuisine = view.findViewById(R.id.tv_cuisine);
+        tv_festivals = view.findViewById(R.id.tv_festivals);
+        tv_kungfu = view.findViewById(R.id.tv_kungfu);
+
+        apiGetChinese("Chinese Cities");
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v == tv_cities) {
+                    apiGetChinese("Chinese Cities");
+                } else if (v == tv_cuisine) {
+                    apiGetChinese("Chinese Food");
+                } else if (v == tv_festivals) {
+                    apiGetChinese("Chinese Carnivals");
+                } else if (v == tv_kungfu) {
+                    apiGetChinese("Chinese Kungfu");
+                }
+            }
+        };
+
+        tv_cities.setOnClickListener(onClickListener);
+        tv_cuisine.setOnClickListener(onClickListener);
+        tv_festivals.setOnClickListener(onClickListener);
+        tv_kungfu.setOnClickListener(onClickListener);
     }
 
-    private void apiPhotoList() {
-        RetrofitHttp.photoService.getPhotos(++currentPage, perPage)
-                .enqueue(new Callback<ArrayList<Photo>>() {
-                    @Override
-                    public void onResponse(Call<ArrayList<Photo>> call, Response<ArrayList<Photo>> response) {
-                        ArrayList<Photo> body = response.body();
-                        if (body != null) {
-                            refreshAdapter(body);
-                        }
-                    }
+    private void apiGetChinese(String query) {
+        RetrofitHttp.photoService.searchPhoto(query, 1, 40).enqueue(new Callback<Search>() {
+            @Override
+            public void onResponse(@NonNull Call<Search> call, @NonNull Response<Search> response) {
+                Search body = response.body();
+                if (body != null) {
+                    refreshAdapter((ArrayList<Photo>) body.getResults());
+                }
+            }
 
-                    @Override
-                    public void onFailure(Call<ArrayList<Photo>> call, Throwable t) {
-                        Log.e("@@@onFailure", t.getMessage());
-                    }
-                });
+            @Override
+            public void onFailure(@NonNull Call<Search> call, @NonNull Throwable t) {
+                Log.d("@@@", "Search -> " + t.getLocalizedMessage());
+            }
+        });
     }
+
 
     private void refreshAdapter(ArrayList<Photo> items) {
         adapter = new HomeAdapter(this, items);
